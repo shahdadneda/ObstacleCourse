3fbf6f210e761fb08e9d7e330bbf65b2997dae6d
Shahdad Neda <shahdad.neda@gmail.com>
2024-08-19 23:58:27 -0700

    first commit for unity

---
+++ Assets/Mover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstruction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        float xValue = Input.GetAxis("Horizontal") *Time .deltaTime *moveSpeed;
+        float zValue = Input.GetAxis("Vertical") *Time.deltaTime *moveSpeed;
+
+        transform.Translate(xValue,0,zValue);
+    }
+
+    void PrintInstruction() {
+        Debug.Log("Welcome to this Obstical Crouse!");
+        Debug.Log("Control your player with WASD");
+        Debug.Log("Dont hit the walls!");
+    }
+    
+
+}
